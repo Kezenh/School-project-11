024fa1fea25d131c0b17166cba676ea28841b653
Kezenh <quentin.rampteau@gmail.com>
2026-09-07 15:52:37 +0200

    Adding basename to router

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/School-project-11">
       <Header />
       <Routes>
         <Route exact path='/' element={<Home />} />
